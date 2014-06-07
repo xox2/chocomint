0595c2a8060da591aa4e3d74dffcbf6216525b25
toromoti <toromoti@gmail.com>
2014-06-07 11:41:57 +0000

    Power up!!

--- tests/test01.sh
+++ tests/test01.sh
@@ -20,29 +20,35 @@ false #ただのcommentだよ
 
 for i in 1 2 3
 do
-  echo \'bdbsd #:nonono
-  echo \"fasdfasd
-  #:oodfipasid
+  echo \'bdbsd $i #:nonono $i
+  echo \"fasdfasd $i
+  #:oodfipasid $i
 done
 
 echo "pipe command" | cat
 
 str=`echo subshell zxc | cat`
 str=$(echo subshell qwe | cat)
-echo $str
+echo "str=\"$str\"" #: $str
+echo "str=\'$str\'" #: $str
 
 true \
   true
 
+true \
+#: backslash newline test
+true \
+  #: backslash whitespace newline test
+
 multiple_line='a #:
 sadasdasdasda\
-adsa #: asdasd'
-echo $multi_line #:    status=0    match='ads'
+adsa #: asdasd'     #:    status=0
+echo $multiple_line #:    status=0    match='ads'
 
 echo 'a #: sadasdasdasdsdfashfljsdlfhashasklhfljlksdhflkjahsdflkhaslkfhasolkjfaasdasdasd'
-#:    status=0    match='ads'
+	#:    status=0    match='ads'
 
-echo '342 #:
+     echo '342 #:
 1231234324
 2343452345' #: status=0 match='2343'
 

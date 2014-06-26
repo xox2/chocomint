977b9262d92a6d69685e34221938dd87d38f79a1
toromoti <toromoti@gmail.com>
2014-06-26 16:16:10 +0000

    match-test evaluates both stdout and stderr. and, clean the various parts

--- tests/test.sh
+++ tests/test.sh
@@ -2,19 +2,22 @@
 # vim: ft=sh
 
 true No.1
-#: status:0
+#: status:0 status!:10 status:1
 
 . tests/dummy   #: status:0
 
 false #: status:1
 
+echo "error out" >&2
+#: match:'error' match!:'error' match:'success'
+
 echo 1234 #: status:0 match:'1234'
 
-false # Just a comment
+sleep 5; false #: status:1
 
 for i in 1 2 3
 do
-  echo count $i #: match:"$i"
+  echo "count $i" #: match:"$i"
 done
 
 echo "pipe" | cat
@@ -26,9 +29,9 @@ str=$(echo 10 | cat)
 echo "str=$str"
 #: $str='10' $str!=100 match:'str'
 
-multiple_line='a
-b
-c' #: status:0
+multiple_line='line one: a
+line two: b
+line three: c' #: status:0
 
 echo "$multiple_line" #: status:0 match:'b'
 

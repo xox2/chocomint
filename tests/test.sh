f536e4d2732901a5afa71b9a29d375f94471f26f
toromoti <toromoti@gmail.com>
2014-11-27 09:27:40 +0000

    Change to allow describe test-comment over multiple lines

--- tests/test.sh
+++ tests/test.sh
@@ -15,10 +15,13 @@ true #: status:0
 echo "Hello"
 #: stdout:'hello' status!:1
 
-dummy_func
-#: stderr:'error' stdout:~'.*std.*' status:3 status!:0
+dummy_func  #: stderr:'error' stdout:~'.*std.*'
+            #: status:3       status!:0
 
 for i in 1 2 3
 do
-  echo $i #: stdout:"$i"
+  echo $i
+  #: stdout:"$i"
+  #: stdout:"1"
+  #: status!:1
 done

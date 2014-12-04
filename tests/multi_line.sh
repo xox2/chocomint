dd53b7ed0caf04952f76ee03c3e8a739b2761704
toromoti <toromoti@gmail.com>
2014-12-04 18:36:44 +0000

    stage 0.3.7-dev

---
+++ tests/multi_line.sh
@@ -0,0 +1,10 @@
+a="SIZE is a integer and optional unit (example: 10M is 10*1024*1024).  Unis
+are K, M, G, T, P, E, Z, Y (powers of 1024) or KB, MB, ... (powers of 1000)"
+
+echo "SIZE is an integer and optional unit (example: 10M is 10*1024*1024).  Units
+are K, M, G, T, P, E, Z, Y (powers of 1024) or KB, MB, ... (powers of 1000).
+
+Using color to distinguish file types is disabled both by default and
+with --color=never.  With --color=auto, ls emits color codes only when
+standard output is connected to a terminal.  The LS_COLORS environment
+variable can change the settings.  Use the dircolors command to set it." #: output:"$a"
